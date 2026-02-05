d0dcb38644f4e8a712ab828289dd872ab2da2814
Shafain <raul.ibrahimov.359@gmail.com>
2026-02-06 02:07:46 +0300

    Add Tween node and movement animations

--- Scripts/Piece.gd
+++ Scripts/Piece.gd
@@ -1,9 +1,16 @@
 extends Node2D
 
 export (String) var color;
+var move_tween;
 
 func _ready():
-	pass # Replace with function body.
+	move_tween = get_node("move_tween");
+	
+func move(target):
+	move_tween.interpolate_property(self, "position", position, target, .3, Tween.TRANS_SINE, Tween.EASE_OUT);
+	move_tween.start();
+	pass;
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
